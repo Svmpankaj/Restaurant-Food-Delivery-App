f027a7f14ba67c41456b728358b54baf104af06b
Svmpankaj <svmpankaj@gmail.com>
2023-08-03 23:16:54 +0530

    adding menu page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2">
+      {/* LOGO  */}
+      <div>
+        <Link href="/"> MaaKali </Link>
+      </div>
+      {/* MOBILE MINU */}
+      <div>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
