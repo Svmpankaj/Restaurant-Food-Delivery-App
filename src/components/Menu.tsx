c8efa718c16341bcbe374ed2f142523c6493458f
Svmpankaj <svmpankaj@gmail.com>
2023-08-05 23:51:00 +0530

    adding open close menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -8,9 +8,21 @@ const Menu = () => {
   return (
     <div>
       {!open ? (
-        <Image src="/open.png" alt="" width={20} height={20} />
+        <Image
+          src="/open.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setOpen(true)}
+        />
       ) : (
-        <Image src="/close.png" alt="" width={20} height={20} />
+        <Image
+          src="/close.png"
+          alt=""
+          width={20}
+          height={20}
+          onClick={() => setOpen(false)}
+        />
       )}
     </div>
   );
