7f4f2e5ef885b9d2f56b6e63b321215e4635a43b
Svmpankaj <svmpankaj@gmail.com>
2023-08-15 23:53:55 +0530

    set left and right menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Menu from "./Menu";
 const Navbar = () => {
   return (
     <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+      {/* LEFT LINK  */}
       <div className="hidden md:flex gap-4">
         <Link href="/">Homepage</Link>
         <Link href="/menu">Menu</Link>
@@ -15,8 +16,15 @@ const Navbar = () => {
         <Link href="/">MaaKAli</Link>
       </div>
       {/* MOBILE MENU */}
-      <div>
+      <div className="md:hidden">
         <Menu />
+      </div>
+      {/* RIGHT LINK  */}
+      <div className="hidden md:flex gap-4">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Homepage</Link>
       </div>
     </div>
   );
