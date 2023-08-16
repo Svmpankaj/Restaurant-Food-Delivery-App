b40124c466c280b14b5a5b722ba1a6a628e3aecf
Svmpankaj <svmpankaj@gmail.com>
2023-08-16 23:07:24 +0530

    set menu for middle screen

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase">
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-200">
       {/* LEFT LINK  */}
-      <div className="hidden md:flex gap-4">
+      <div className="hidden md:flex gap-4 flex-1">
         <Link href="/">Homepage</Link>
         <Link href="/menu">Menu</Link>
-        <Link href="/">Homepage</Link>
-        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
       </div>
       {/* LOGO */}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">MaaKAli</Link>
       </div>
       {/* MOBILE MENU */}
@@ -20,11 +22,17 @@ const Navbar = () => {
         <Menu />
       </div>
       {/* RIGHT LINK  */}
-      <div className="hidden md:flex gap-4">
-        <Link href="/">Homepage</Link>
-        <Link href="/menu">Menu</Link>
-        <Link href="/">Homepage</Link>
-        <Link href="/">Homepage</Link>
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <Image src="/phone.png" alt="" width={20} height={20} />
+          <span>123 456 78</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <CartIcon />
       </div>
     </div>
   );
