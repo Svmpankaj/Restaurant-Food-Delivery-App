e49b5a463f55640fc969e4460359fbf3e6303cba
Svmpankaj <svmpankaj@gmail.com>
2023-08-20 23:21:36 +0530

    image slider is added

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const data = [
   {
@@ -15,24 +16,38 @@ const data = [
   {
     id: 3,
     title: "the best pizza to share with your family",
-    image: "/slide3.png",
+    image: "/slide3.jpg",
   },
 ];
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () =>
+        setCurrentSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1)),
+      2000
+    );
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
       {/* TEXT CONTAINER  */}
       <div className="h-1/2 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
         <h1 className="text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl">
-          Test
+          {data[currentSlide].title}
         </h1>
         <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
       </div>
       {/* IMAGE CONTAINER  */}
       <div className="w-full h-1/2 relative">
-        <Image src="/slide1.png" alt="" fill className="object-cover" />
+        <Image
+          src={data[currentSlide].image}
+          alt=""
+          fill
+          className="object-cover"
+        />
       </div>
     </div>
   );
