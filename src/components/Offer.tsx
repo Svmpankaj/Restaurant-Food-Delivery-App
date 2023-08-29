33dfbd9a602ed8f501f92ca80a23dce4b3212eb7
Svmpankaj <svmpankaj@gmail.com>
2023-08-29 23:49:19 +0530

    adding countdown at offer

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import CountDown from "./CountDown";
 
 const Offer = () => {
   return (
@@ -13,6 +14,7 @@ const Offer = () => {
           Progressively simplify effective e-toilers and process-centric methods
           of empowerment. Quickly pontificate parallel.
         </p>
+        <CountDown />
         <button className="bg-red-500 text-white rounded-md py-3 px-6">
           Order Now
         </button>
