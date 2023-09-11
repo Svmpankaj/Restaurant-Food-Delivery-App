2b123e1d7cfda9213976bbea57d8b8c2388da020
Svmpankaj <svmpankaj@gmail.com>
2023-09-11 21:08:06 +0530

    single page order is done

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Props = {
   price: number;
@@ -13,6 +13,12 @@ const Price = ({ price, id, options }: Props) => {
   const [quantity, setQuantity] = useState(1);
   const [selected, setSelected] = useState(0);
 
+  useEffect(() => {
+    setTotal(
+      quantity * (options ? price + options[selected].additionalPrice : price)
+    );
+  }, [quantity, selected, options, price]);
+
   return (
     <div className="flex flex-col gap-4">
       <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
@@ -21,10 +27,12 @@ const Price = ({ price, id, options }: Props) => {
         {options?.map((option, index) => (
           <button
             key={option.title}
-            className="p-2 ring-1 ring-red-400 rounded-md"
+            className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md"
             style={{
               background: selected === index ? "rgb(248 113 113)" : "white",
+              color: selected === index ? "white" : "red",
             }}
+            onClick={() => setSelected(index)}
           >
             {option.title}
           </button>
@@ -35,9 +43,17 @@ const Price = ({ price, id, options }: Props) => {
         <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
           <span>Quantity</span>
           <div className="flex gap-4 items-center">
-            <button>{"<"}</button>
+            <button
+              onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
+            >
+              {"<"}
+            </button>
             <span>{quantity}</span>
-            <button>{">"}</button>
+            <button
+              onClick={() => setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}
+            >
+              {">"}
+            </button>
           </div>
         </div>
         {/* CART BUTTON  */}
