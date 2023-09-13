1a12a03d709205a9ae0b9d269555f8fea10cb66a
Svmpankaj <svmpankaj@gmail.com>
2023-09-13 23:46:43 +0530

    cartpage set for xl and 2xl screen

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const CartPage = () => {
   return (
-    <div className="h-[calc(100vh - 6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
+    <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
       {/* PRODUCT CONTAINER */}
-      <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full">
+      <div className="h-1/2 p-4 flex flex-col justify-center lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
         {/* SINGLE ITEM */}
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
@@ -16,6 +16,7 @@ const CartPage = () => {
           <h2 className="font-bold">$79.90</h2>
           <span className="cursor-pointer">X</span>
         </div>
+        {/* SINGLE ITEM */}
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
@@ -25,6 +26,7 @@ const CartPage = () => {
           <h2 className="font-bold">$79.90</h2>
           <span className="cursor-pointer">X</span>
         </div>
+        {/* SINGLE ITEM */}
         <div className="flex items-center justify-between mb-4">
           <Image src="/temporary/p1.png" alt="" width={100} height={100} />
           <div className="">
@@ -37,7 +39,7 @@ const CartPage = () => {
       </div>
 
       {/* PAYMENT CONTAINER */}
-      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center">
+      <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
         <div className="flex justify-between">
           <span className="">Subtotal (3 times)</span>
           <span className="">$81.70</span>
