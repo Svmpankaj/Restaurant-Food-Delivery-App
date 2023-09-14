58d34aab066157c16f81c853a2fbdd7c40b79d28
Svmpankaj <svmpankaj@gmail.com>
2023-09-14 21:24:59 +0530

    adding login page

--- src/app/login/login.tsx
+++ src/app/login/login.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
 import React from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="">
+      {/* BOX */}
+      <div className="">
+        {/* IMAGE CONTAINER */}
+        <div className="relative">
+          <Image src="/loginBg.png" alt="" fill />
+        </div>
+        {/* FORM CONTAINER */}
+        <div className="">
+          <h1>Welcome</h1>
+          <p>Log into your account or create a new one using social buttons</p>
+          <button className="">
+            <Image src="/googleLogo.png" alt="" fill />
+            <p>Login with google account</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
