37c5dfe73b8b6f5b9877ffba50289b83e8f84610
Svmpankaj <svmpankaj@gmail.com>
2023-09-15 23:22:33 +0530

    adding login with facebook

--- src/app/login/login.tsx
+++ src/app/login/login.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
   return (
-    <div className="">
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/* BOX */}
-      <div className="">
+      <div className="h-full shadow-2xl">
         {/* IMAGE CONTAINER */}
         <div className="relative">
           <Image src="/loginBg.png" alt="" fill />
@@ -14,10 +15,29 @@ const LoginPage = () => {
         <div className="">
           <h1>Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className="">
-            <Image src="/googleLogo.png" alt="" fill />
-            <p>Login with google account</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+            <Image
+              src="/google.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Google</span>
           </button>
+          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+            <Image
+              src="/facebook.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Facebook</span>
+          </button>
+          <p>
+            Have a problem?<Link href="/">Contact us</Link>
+          </p>
         </div>
       </div>
     </div>
